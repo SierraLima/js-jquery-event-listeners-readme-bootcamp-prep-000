b041e4e63f28813b31d150cbcbdc4c930f6c060f
Stéphane <stephluethi@hotmail.com>
2018-09-03 14:10:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on("keydown", function(key){
+  $('#typing').on("keydown", function(key){
     if (key.which == 103) {
       alert('')
     }
